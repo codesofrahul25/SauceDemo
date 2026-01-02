ee0619d519fe343cb3bd282e077f8ea795253853
codesofrahul25 <ramluhar25@gmail.com>
2026-01-02 12:13:10 +0530

    Update TestNGTestRunner.java

--- src/test/java/TestRunner/TestNGTestRunner.java
+++ src/test/java/TestRunner/TestNGTestRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.testng.CucumberOptions;
 glue="StepDefinitions", 
 monochrome=true,
 dryRun=false,
-tags = "@ddt",
+tags = "@checkout",
 plugin = {"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 
 
